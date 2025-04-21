4eb74b3182b0aa984d047774449e34cac1f800a7
bindingofdata <hcurren82@gmail.com>
2025-04-21 16:31:37 -0700

    Updated starting location values for player and ship.

--- listing1-1.py
+++ listing1-1.py
@@ -5,14 +5,14 @@
 WIDTH = 800
 HEIGHT = 600
 
-player_x = 600
+player_x = 650
 player_y = 350
 
 def draw():
     screen.blit(images.backdrop, (0, 0))
     screen.blit(images.mars, (50, 50))
     screen.blit(images.astronaut, (player_x, player_y))
-    screen.blit(images.ship, (130, 150))
+    screen.blit(images.ship, (600, 300))
 
 def game_loop():
     global player_x, player_y
