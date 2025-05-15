8fbde304f01dc68d57977ca2aa0c26e0e8ca4604
bindingofdata <hcurren82@gmail.com>
2025-05-14 18:45:22 -0700

    Completed base game map code.

--- listing4.py
+++ listing4.py
@@ -11,7 +11,7 @@ import time, random, math
 WIDTH = 800
 HEIGHT = 800
 
-STARTING_ROOM = 32
+STARTING_ROOM = 31
 
 #PLAYER variables
 PLAYER_NAME = "Cloud"
@@ -22,7 +22,7 @@ current_room = STARTING_ROOM
 top_left_x = 100
 top_left_y = 150
 
-DEMO_OBJECTS = [images.floor, images.pillar, images.soil]
+#DEMO_OBJECTS = [images.floor, images.pillar, images.soil]
 
 #############
 ##   MAP   ##
@@ -40,4 +40,32 @@ for planetsectors in range(1, 26): #Rooms 1 to 25 are generated here
 
 GAME_MAP += [
     #["Room Name", height, width, Top exit?, Bottom exit?]
+    ["The airlock", 13, 5, True, False], # room 26
+    ["The engineering lab", 13, 13, False, False], # room 27
+    ["Poodle Mission Control", 9, 13, False, True], # room 28
+    ["The viewing gallery", 9, 15, False, False], # room 29
+    ["The crew's bathroom", 5, 5, False, False], # room 30
+    ["The airlock entry bay", 7, 11, True, True], # room 31
+    ["Left elbow room", 9, 7, True, False], # room 32
+    ["Right elbow room", 7, 13, True, True], # room 33
+    ["The sciencelab", 13, 13, False, True], # room 34
+    ["The greenhouse", 13, 13, True, False], # room 35
+    [PLAYER_NAME + "'s sleeping quarters", 9, 11, True, False], # room 36
+    ["West corridor", 15, 5, True, True], # room 37
+    ["The briefing room", 7, 13, False, True], # room 38
+    ["The crew's community room", 11, 13, True, False], # room 39
+    ["Main Mission Control", 14, 14, False, False], # room 40
+    ["The sick bay", 12, 7, True, False], # room 41
+    ["West corridor", 9, 7, True, False], # room 42
+    ["Utilities control room", 9, 9, False, True], # room 43
+    ["Systems engineering bay", 9, 11, False, False], # room 44
+    ["Security portal to Mission Control", 7, 7, True, False], # room 45
+    [FRIEND1_NAME + "'s sleeping quarters", 9, 11, True, True], # room 46
+    [FRIEND2_NAME + "'s sleeping quarters", 9, 11, True, True], # room 47
+    ["The pipeworks", 13, 11, True, False], # room 48
+    ["The chief scientist's office", 9, 7, True, True], # room 49
+    ["The robot workshop", 9, 11, True, False], # room 50
 ]
+
+# simple sanity check on the map above to check data entry
+assert len(GAME_MAP) - 1 == MAP_SIZE, "Map size and GAME_MAP don't match."
